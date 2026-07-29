14a62ceced1c13abc04cc2538129cd3c99afcb70
YACIINE105 <mooml.intern@gmail.com>
2026-07-29 17:03:42 +0300

    making file id optional for processing endpoint

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
 from typing import Optional
 class ProcessRequest(BaseModel):
-    file_id : str
+    #file_id is now optional if got it process it else procedss all file in the folder
+    file_id : str = None
     chunk_size : Optional[int]=100
     overlap_size:Optional[int]=20
     reset:Optional[int]=0
