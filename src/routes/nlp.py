7b515a4666b38772e66021a22b05b978e0648acc
YACIINE105 <mooml.intern@gmail.com>
2026-08-10 11:25:18 +0300

    ADDED THE ANSWER GENERATION BASED ON THE CHUNKS

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -106,7 +106,8 @@ async def search_index(request:Request, project_id:str, search_request:SearchReq
 
     nlp_controller = NLPController(vector_db_client=request.app.vector_db_client,
                                 generation_client=request.app.generation_client,
-                                embedding_client=request.app.embedding_client)
+                                embedding_client=request.app.embedding_client,
+                                template_parser=request.app.template_parser,)
     
     indexed_vectors =  nlp_controller.search_vector_db_collection(project=project,
                                                                   text=search_request.text,
@@ -125,3 +126,39 @@ async def search_index(request:Request, project_id:str, search_request:SearchReq
                                  "indexed_vectors":indexed_vectors
                                 }
                 )
+    
+    
+
+@nlp_router.get("/index/answer/{project_id}")
+async def answer_index(request:Request, project_id:str, search_request:SearchRequest):
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+
+    if not project:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"signal":ResponseSignal.PROJECT_NOT_FOUND_ERROR.value})
+
+    nlp_controller = NLPController(vector_db_client=request.app.vector_db_client,
+                                generation_client=request.app.generation_client,
+                                embedding_client=request.app.embedding_client,
+                                template_parser=request.app.template_parser,)
+    
+    answer, full_prompt, chat_history = nlp_controller.answer_rag_query(project=project,
+                                    query = search_request.text,
+                                    limit=search_request.limit)
+    
+    if not answer:
+        return JSONResponse(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    content={"signal":ResponseSignal.RAG_ANSWER_FAILED.value})  
+        
+    return JSONResponse(status_code=status.HTTP_200_OK,
+                        content={"signal" : ResponseSignal.RAG_ANSWER_SUCCESS.value,
+                                 "Answer" : answer,
+                                 "full_prompt" : full_prompt,
+                                 "chat_history" : chat_history
+                                }
+                )
+    
+    
